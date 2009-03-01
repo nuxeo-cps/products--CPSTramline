e02e29490849a13b431dc234ea1c87a13682cae9
gracinet <devnull@localhost>
2009-03-01 14:41:00 +0000

    dtml-isation of gp_upload resources

--- skins/gp_uppload/jquery.fileupload.auto.js
+++
@@ -1,8 +0,0 @@
-if (typeof(jQuery)!='undefined')
-    jQuery(document).ready(function() {
-        jQuery('form[enctype^="multipart/form-data"]').each(function(){
-            if (jQuery('input:file', this).length > 0) {
-                jQuery(this).fileUpload({use_iframes:false});
-            }
-        });
-    });
